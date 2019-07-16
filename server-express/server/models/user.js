3923f827168ad5fba6063542aba2fd5e0e9b851e
Rodrigo M <rodrigo.medina.neri@gmail.com>
2019-07-15 23:13:51 -0300

    Added password to model in User

--- server-express/server/models/user.js
+++ server-express/server/models/user.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: DataTypes.STRING,
     role: DataTypes.INTEGER
   }, {});
   User.associate = function(models) {
